e39a3bfdfb458727122d09f40c5dd64d7579cde8
harish_selva <97356861+Harishraj16@users.noreply.github.com>
2023-08-26 15:54:59 +0530

    Add files via upload

---
+++ Learning/3. Conditional statements.py
@@ -0,0 +1,58 @@
+# If Else Statement in python
+
+'''
+Write a program to check the given number is even or even
+'''
+
+# =============================================================================
+# n=int(input("Enter the number:"))
+# if n%2==0:
+#     print(n,"is Even")
+# else:
+#     print(n,"is Odd")
+# =============================================================================
+
+
+
+'''
+WAP to check Vote eligibility by enter his/her name and age
+'''
+
+# =============================================================================
+# name=input("Enter your name:")
+# age=int(input("Enter your age:"))
+# if age>=18:
+#     print("You are eligible", name)
+# else:
+#     print("You are not eligible",name)
+# =============================================================================
+
+
+
+'''
+WAP to display the fine amount in library
+
+Days   fine
+0      No fine
+1-5    0.5
+6-10   1
+11-30  5
+>30    Membership Cancel
+'''
+
+
+# =============================================================================
+# days=int(input("enter the no of days delayed: "))
+# if days==0:
+#     print("no fine")
+# elif days>0 and days<6:
+#     print("rs:",days*0.5)
+# elif days>5 and days<11:
+#     print("rs:",days*1)
+# elif days>10 and days<31:
+#     print("rs:",days*5)
+# elif days>30:
+#     print("membership cancelled")
+# else:
+#     print("days invalid")
+# =============================================================================
