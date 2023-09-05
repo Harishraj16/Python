e7838d2ef9561afc486e57c372130ae206a4993f
harish_selva <97356861+Harishraj16@users.noreply.github.com>
2023-09-05 19:03:21 +0530

    Add files via upload

---
+++ Learning/13. Math Functions.py
@@ -0,0 +1,14 @@
+#Math Functions
+
+import math as mt  
+
+print("Sq.root:",int(mt.sqrt(4)))
+print("Ceil:",mt.ceil(1.55))
+print("Floor:",mt.floor(1.55))
+print("Factorial:",mt.factorial(5))
+print("Absolute: ",mt.fabs(-5))
+print("Power:",mt.pow(2, 2))
+print("Log:",mt.log2(2))
+print("Log:",mt.log10(2))
+print("pi:",mt.pi)
+print("e:",mt.e)
